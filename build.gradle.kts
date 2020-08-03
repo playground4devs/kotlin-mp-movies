88cb28da513870d7e206f067f99169372a07cc8c
Omar Miatello <jackl85@gmail.com>
2020-08-03 11:32:42 +0200

    First commit: Kotlin Multiplatform 1.4-RC1

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        maven {
+            url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+        }
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0-rc")
+        classpath("com.android.tools.build:gradle:4.0.1")
+    }
+}
+group = "com.github.omarmiatello"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+        jcenter()
+        maven {
+            url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+        }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
+                useModule("com.android.tools.build:gradle:4.0.1")
+            }
+        }
+    }
+}
+rootProject.name = "kmp2"
+
+
+include(":shared")
+include(":androidApp")
+
