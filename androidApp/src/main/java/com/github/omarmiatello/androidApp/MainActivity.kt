2a3a7348e0be4f271e2f6118e115ca9f9ae98d75
Omar Miatello <jackl85@gmail.com>
2020-08-03 12:12:50 +0200

    Add Sample.kt (actual and expect)

--- androidApp/src/main/java/com/github/omarmiatello/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/github/omarmiatello/androidApp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.github.omarmiatello.androidApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import sample.Platform
+import sample.Sample
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView).text = "Hello from ${Platform.name}"
     }
 }
---
+++ shared/src/androidTest/kotlin/sample/SampleTestsAndroid.kt
@@ -0,0 +1,13 @@
+package sample
+
+// TODO fix compilation errors
+
+//import kotlin.test.Test
+//import kotlin.test.assertTrue
+//
+//class SampleTestsAndroid {
+//    @Test
+//    fun testHello() {
+//        assertTrue("Android" in hello())
+//    }
+//}
