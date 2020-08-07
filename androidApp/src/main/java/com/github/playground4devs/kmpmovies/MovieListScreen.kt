fd40dd67c17574f8f0e8d4852eb6cef4b9a40ad2
fabioCollini <fabio.collini@gmail.com>
2020-08-07 12:49:09 +0200

    Add tablet layout

--- androidApp/src/main/java/com/github/playground4devs/kmpmovies/MovieListScreen.kt
+++ androidApp/src/main/java/com/github/playground4devs/kmpmovies/MovieListScreen.kt
@@ -1,23 +1,41 @@
 package com.github.playground4devs.kmpmovies
 
 import androidx.compose.Composable
+import androidx.ui.core.ContextAmbient
+import androidx.ui.core.DensityAmbient
+import androidx.ui.core.Modifier
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.drawBackground
 import androidx.ui.foundation.lazy.LazyColumnItems
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
+import androidx.ui.material.Divider
 import androidx.ui.material.ListItem
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import coil.request.GetRequest
+import coil.size.Precision
+import coil.size.Scale
 import com.github.playground4devs.kmpmovies.ui.MainScreen
-import com.github.playground4devs.movies.Lce
-import com.github.playground4devs.movies.ModelSamples
-import com.github.playground4devs.movies.Movie
+import com.github.playground4devs.movies.*
+import com.github.playground4devs.movies.MovieImagePosition.LEFT
+import com.github.playground4devs.movies.MovieImagePosition.TOP
+import dev.chrisbanes.accompanist.coil.CoilImage
+
 
 @Composable
 fun MovieListScreen(movieList: Lce<List<Movie>>) =
     MainScreen("Popular Movies & Series") { innerPadding ->
         val navigation = getNavigation()
         when (movieList) {
-            is Lce.Success -> LazyColumnItems(movieList.data) { movie ->
-                MovieItem(movie, onClickMovie = { navigation.navigateTo(Screen.Detail(it)) })
-            }
+            is Lce.Success -> if (isTablet())
+                CardLayout(
+                    movieList.data.toCards(),
+                    onClickMovie = { navigation.navigateTo(Screen.Detail(it)) })
+            else
+                LazyColumnItems(movieList.data) { movie ->
+                    MovieItem(movie, onClickMovie = { navigation.navigateTo(Screen.Detail(it)) })
+                }
             is Lce.Loading -> {
                 Text("Loading...")
             }
@@ -27,18 +45,109 @@ fun MovieListScreen(movieList: Lce<List<Movie>>) =
         }
     }
 
+@Composable
+fun isTablet() =
+    with(DensityAmbient.current) {
+        ContextAmbient.current.resources.displayMetrics.widthPixels > 600.dp.toPx()
+    }
+
+@Composable
+private fun CardLayout(
+    card: Card,
+    onClickMovie: (Movie) -> Unit
+) {
+    when (card) {
+        is RootCard ->
+            LazyColumnItems(card.cards) { group ->
+                CardLayout(group, onClickMovie)
+                Divider(
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                )
+            }
+        is HorizontalGroupCard ->
+            Row {
+                card.cards.forEach { group ->
+                    Column(modifier = Modifier.weight(weight = group.weight)) {
+                        CardLayout(group, onClickMovie)
+                    }
+                }
+            }
+        is VerticalGroupCard ->
+            card.cards.forEachIndexed { index, group ->
+                if (index > 0) {
+                    Divider(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp))
+                }
+                CardLayout(group, onClickMovie)
+            }
+        is MovieCard -> MovieCardItem(card, onClickMovie)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1024)
+@Composable
+fun MovieListScreenPreviewTablet() = MovieListScreen(Lce.Success(ModelSamples.movies))
+
 @Preview(showBackground = true)
 @Composable
 fun MovieListScreenPreview() = MovieListScreen(Lce.Success(ModelSamples.movies))
 
 @Composable
-fun MovieItem(movie: Movie, onClickMovie: (Movie) -> Unit = {}) = ListItem(
-    text = { Text(movie.title) },
-    overlineText = { Text(movie.genres.joinToString()) },
-    secondaryText = { Text(movie.plot) },
-    trailing = { Text("⭐️ ${movie.rating ?: "N/A"}") },
-    onClick = { onClickMovie(movie) }
-)
+fun MovieItem(movie: Movie, showPlot: Boolean = true, onClickMovie: (Movie) -> Unit = {}) =
+    ListItem(
+        text = { Text(movie.title) },
+        overlineText = {
+            Text(movie.genres.joinToString() + " ⭐️ ${movie.rating ?: "N/A"}")
+        },
+        secondaryText = {
+            if (showPlot) {
+                Text(movie.plot)
+            }
+        },
+        onClick = { onClickMovie(movie) }
+    )
+
+@Composable
+fun MovieCardItem(card: MovieCard, onClickMovie: (Movie) -> Unit = {}) {
+    if (card.imagePosition == LEFT) {
+        Row {
+            Column {
+                card.movie.image?.let {
+                    CoilImage(
+                        modifier = Modifier.padding(top = 16.dp, start = 16.dp),
+                        request = GetRequest.Builder(ContextAmbient.current)
+                            .data(it.url)
+                            .size(200, 200)
+                            //                        .size(it.width, it.height)
+                            .precision(Precision.EXACT)
+                            .scale(Scale.FILL)
+                            .build()
+                    )
+                }
+            }
+            Column {
+                MovieItem(card.movie, card.showPlot, onClickMovie)
+            }
+        }
+    } else {
+        if (card.imagePosition == TOP) {
+            card.movie.image?.let {
+                CoilImage(
+                    modifier = Modifier.padding(16.dp).fillMaxWidth().aspectRatio(1.5f)
+                        .drawBackground(Color.Red),
+                    request = GetRequest.Builder(ContextAmbient.current)
+                        .data(it.url)
+//                        .size(600, 600)
+                        //                        .size(it.width, it.height)
+                        .precision(Precision.INEXACT)
+                        .scale(Scale.FIT)
+                        .build()
+                )
+            }
+        }
+        MovieItem(card.movie, card.showPlot, onClickMovie)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
--- shared/src/commonMain/kotlin/com/github/playground4devs/movies/Models.kt
+++ shared/src/commonMain/kotlin/com/github/playground4devs/movies/Models.kt
@@ -19,3 +19,68 @@ data class Movie(
     val genres: List<String>,
     val releaseDate: Long?
 )
+
+sealed class Card {
+    abstract val weight: Float
+}
+
+data class RootCard(
+    val cards: List<Card>,
+    override val weight: Float = 1f
+) : Card() {
+    constructor(vararg cards: Card) : this(cards.toList())
+}
+
+data class HorizontalGroupCard(
+    val cards: List<Card>,
+    override val weight: Float = 1f
+) : Card() {
+    constructor(vararg cards: Card) : this(cards.toList())
+}
+
+data class VerticalGroupCard(
+    val cards: List<Card>,
+    override val weight: Float = 1f
+) : Card() {
+    constructor(vararg cards: Card, weight: Float = 1f) : this(cards.toList(), weight)
+}
+
+enum class MovieImagePosition {
+    LEFT, TOP, NONE
+}
+
+data class MovieCard(
+    val movie: Movie,
+    val imagePosition: MovieImagePosition = MovieImagePosition.NONE,
+    val showPlot: Boolean = true,
+    override val weight: Float = 1f
+) : Card()
+
+fun List<Movie>.toCards(): Card {
+    return RootCard(
+        HorizontalGroupCard(
+            subList(0, 3).map { MovieCard(it, imagePosition = MovieImagePosition.LEFT, showPlot = false) }),
+        HorizontalGroupCard(
+            VerticalGroupCard(
+                HorizontalGroupCard(
+                    VerticalGroupCard(
+                        subList(3, 5).map { MovieCard(it) }
+                    ),
+                    MovieCard(this[6], weight = 2f, imagePosition = MovieImagePosition.TOP)
+                ),
+                MovieCard(this[4]),
+                HorizontalGroupCard(
+                    MovieCard(this[6]),
+                    VerticalGroupCard(
+                        MovieCard(this[6]),
+                        MovieCard(this[7], showPlot = false)
+                    )
+                ),
+                MovieCard(this[9]),
+                weight = 4f
+            ),
+            VerticalGroupCard(subList(10, 13).map { MovieCard(it) })
+        ),
+        VerticalGroupCard(subList(13, size).map { MovieCard(it, imagePosition = MovieImagePosition.TOP) })
+    )
+}
